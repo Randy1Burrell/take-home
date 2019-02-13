214cacbfb17d65e772d8f091902da9cb82964357
Randy Burrell <randy1burrell@yahoo.com>
2019-02-13 09:04:25 -0500

    Added reducer file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
+import SubmitReducer from './submitReducer'
 
 const allReducers = combineReducers({
+    submit: SubmitReducer,
 })
 
 export default allReducers;
