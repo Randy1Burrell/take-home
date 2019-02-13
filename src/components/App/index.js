ed8f865c385a45ba36d032b682e0926f29e08e12
Randy Burrell <randy1burrell@yahoo.com>
2019-02-13 08:42:10 -0500

    Setting up for redux

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          Hello, World
-      </div>
+        <BrowserRouter>
+              <div className="container">
+                  Hello, World
+              </div>
+        </BrowserRouter>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import * as serviceWorker from './serviceWorker';
+import App from './components/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux';
+import allReducers from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(allReducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
